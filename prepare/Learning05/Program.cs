39d8b6360c16c41df106dd82a3392f83ab15c854
sydneybohl <boh23006@byui.edu>
2024-05-23 07:44:34 -0500

    shapes activity first commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Red", 3);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Green", 6);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
